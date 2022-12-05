957a8e15a42baa35418d9476196ff78926317840
kawtherbouzeghaya <k_bouze@live.concordia.ca>
2022-12-04 22:37:17 -0500

    Clock Added and modified the output from displaying 1 to 1000

--- src/com/FirdawsBouzeghaya/SchedulerCycle.java
+++ src/com/FirdawsBouzeghaya/SchedulerCycle.java
@@ -4,29 +4,29 @@ public class SchedulerCycle extends Thread{
 
     /* The class time extends from class thread as required in the handout*/
     // Use one binary semaphore that ensures the access to the shared variable time between processes.
-    volatile  static int time; /*we initially start at time =0*/
+    static int time; /*we initially start at time =0*/
+    boolean start_clock;
 
-    public SchedulerCycle() // constructor that has no parameters.
+    public  SchedulerCycle() // constructor that has no parameters.
     {
         time =0;
+        start_clock = false;
     }
-    static public void count()
-    {
-        time++;
-    }
-    public int get_time() throws InterruptedException
+
+    public static int get_time() throws InterruptedException
     {
         return time;
     }
-    static void tick() throws InterruptedException {
+    public static void tick() throws InterruptedException {
 
         Thread.sleep(1000);
-            time++;
+            time=time+1000;
     }
 
     @Override
         public void run()
     {
+
 
             try {
                 tick();
